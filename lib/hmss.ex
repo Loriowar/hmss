4f54c765f8af66830122b4d196f1aec204d9bccd
Ivan Zabrovskiy <loriowar@gmail.com>
2020-02-16 10:43:40 +0300

    initial commit of Home Media Server Streamer

---
+++ lib/hmss.ex
@@ -0,0 +1,18 @@
+defmodule HMSS do
+  @moduledoc """
+  Documentation for HMSS.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HMSS.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hmss_test.exs
@@ -0,0 +1,8 @@
+defmodule HMSSTest do
+  use ExUnit.Case
+  doctest HMSS
+
+  test "greets the world" do
+    assert HMSS.hello() == :world
+  end
+end
