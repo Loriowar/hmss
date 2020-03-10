48f4718667a5f397e0851efcbcc1f9a576ff7959
Ivan Zabrovskiy <loriowar@gmail.com>
2020-03-10 17:01:14 +0300

    FFmpeg stream index
    - add an internal index of each stream into MediaContainer.Info

--- lib/media_container/info.ex
+++ lib/media_container/info.ex
@@ -1,6 +1,9 @@
 defmodule HMSS.MediaContainer.Info do
   def short(path) when is_bitstring(path) do
-    raw_data(path) |> HMSS.MediaContainer.FFprobeExtractor.extract |> Map.merge(%{duration: duration(path)})
+    raw_data(path)
+    |> HMSS.MediaContainer.FFprobeExtractor.extract
+    |> set_ffmpeg_stream_index
+    |> Map.merge(%{duration: duration(path)})
   end
 
   def raw_data(path) do
@@ -11,5 +14,32 @@ defmodule HMSS.MediaContainer.Info do
 
   def duration(path) do
     FFprobe.duration(path)
+  end
+
+  # setup internal index started from zero within each type of stream
+  defp set_ffmpeg_stream_index(short_data) do
+    [:video, :audio, :subtitle]
+    |> Enum.reduce(
+         %{},
+         fn(media_type, result) ->
+           min_index =
+             short_data[media_type]
+             |> Enum.map(fn(el) -> el[:index] end)
+             |> Enum.reject(&is_nil/1)
+             |> Enum.min
+
+           new_media_type =
+             short_data[media_type]
+             |> Enum.map(
+                  fn(el)->
+                    Map.merge(
+                      el,
+                      %{ffmpeg_index: el[:index] - min_index})
+                  end
+                )
+
+           result |> Map.merge(%{media_type => new_media_type})
+         end
+       )
   end
 end
